cf7f3d55ce143cfe17238fde83a04a70972c57f2
sontx13 <sontx13@gmail.com>
2023-08-08 22:20:17 +0700

    connection pool

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,5 +1,20 @@
+const connection = require('../config/database');
+
 const getHomepage = (req,res) =>{
-    res.send('Hello World! aaaaaa');
+    let users = [];
+    // simple query
+    connection.query(
+        'SELECT * FROM Users u',
+        function(err, results, fields) {
+            users =results;
+            //console.log(">>results===="+results); // results contains rows returned by server
+
+            //console.log(">>user===="+users); 
+            res.send(JSON.stringify(users));
+        }
+    );
+
+   
 }
 
 const getSontx13 = (req,res) =>{
