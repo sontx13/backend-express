dc9d36952ffbcfdb730fd480dba4f9cd4ee95100
sontx13 <sontx13@gmail.com>
2023-08-10 19:47:44 +0700

    connection mongooser

--- src/server.js
+++ src/server.js
@@ -9,18 +9,40 @@ const connection = require('./config/database');
 const app = express();
 const port = process.env.PORT||3000;
 const hostname = process.env.HOST_NAME;
+const mongoose = require('mongoose');
 
 //config req.body
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
-
-
 //console.log(process.env);
 //config template engine
 configViewEngine(app);
 //routers
 app.use('/',webRouters );
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+
+const kittySchema = new mongoose.Schema({
+  name: String
+});
+
+const Kitten = mongoose.model('Kitten', kittySchema);
+
+const cat = new Kitten({ name: 'Silence' });
+
+cat.save();
+
+
+( async()=>{
+  try {
+    await connection();
+
+    app.listen(port, () => {
+      console.log(`Example app listening on port ${port}`)
+    })
+  } catch (error) {
+    console.log(`>>> error connection db====`, error)
+  }
+}
+)()
+
+
